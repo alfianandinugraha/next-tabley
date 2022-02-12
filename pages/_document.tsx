2eae403115c66b06eb4d2d4a35dae96482ad6b63
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-12 22:39:03 +0700

    feat(components): create GithubCorner

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+          .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}`,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
