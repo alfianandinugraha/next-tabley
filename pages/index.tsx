af032ffb4452f90e1689d7a9b2e275ac400e2019
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-12 12:20:47 +0700

    feat(pages/index.jsx): create search input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,47 @@
-import { Heading } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { Input } from "@chakra-ui/input";
+import { Box, Heading, HStack } from "@chakra-ui/layout";
 import { Container } from "@chakra-ui/layout";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
 import users from "@data/users";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useTable } from "react-table";
+import { TableInstance, useTable } from "react-table";
+import { User } from "types/model";
 
 const Home: NextPage = () => {
-  const { getTableProps, getTableBodyProps, rows, prepareRow, headerGroups } =
-    useTable({
-      data: users,
-      columns: [
-        {
-          Header: "User ID",
-          accessor: "id",
-        },
-        {
-          Header: "Email",
-          accessor: "email",
-        },
-        {
-          Header: "Fullname",
-          accessor: "fullName",
-        },
-        {
-          Header: "Gender",
-          accessor: "gender",
-        },
-        {
-          Header: "Country",
-          accessor: "country",
-        },
-      ],
-    });
+  const {
+    getTableProps,
+    getTableBodyProps,
+    rows,
+    prepareRow,
+    headerGroups,
+    getHooks,
+  }: TableInstance<User> = useTable({
+    data: users,
+    columns: [
+      {
+        Header: "User ID",
+        accessor: "id",
+      },
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Fullname",
+        accessor: "fullName",
+      },
+      {
+        Header: "Gender",
+        accessor: "gender",
+      },
+      {
+        Header: "Country",
+        accessor: "country",
+      },
+    ],
+  });
 
   return (
     <Container maxW="container.xl" mt="10">
@@ -42,6 +51,13 @@ const Home: NextPage = () => {
       <Heading as="h1" fontSize="3xl">
         Tabley
       </Heading>
+      <Box mt="6" display="flex" justifyContent="space-between">
+        <HStack spacing="3">
+          <Button>Prev</Button>
+          <Button>Next</Button>
+        </HStack>
+        <Input placeholder="Search" maxW="400px" />
+      </Box>
       <Table {...getTableProps()} mt="4">
         <Thead>
           {headerGroups.map((headerGroups) => {
---
+++ types/model.d.ts
@@ -0,0 +1,9 @@
+declare module "types/model" {
+  export type User = {
+    id: string;
+    email: string;
+    fullName: string;
+    gender: string;
+    country: string;
+  };
+}
